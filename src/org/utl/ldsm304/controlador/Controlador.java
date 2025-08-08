167fd524cfba3038e001eb8df7b6ef832d21bc4c
Iris-Ale <irisalegro@gmail.com>
2025-08-08 00:52:34 -0600

    Se generan los metodos de ejercicio 1 completos y main menu

--- src/org/utl/ldsm304/controlador/Controlador.java
+++ src/org/utl/ldsm304/controlador/Controlador.java
@@ -4,6 +4,8 @@
  */
 package org.utl.ldsm304.controlador;
 
+import java.util.Scanner;
+
 /**
  *
  * @author IRIS
@@ -11,10 +13,141 @@ package org.utl.ldsm304.controlador;
 public class Controlador {
 
     //aki metodo ejecutar con menu
+     public void ejecutar() {
+        Scanner leer = new Scanner(System.in);
 
+        
+        //generar arreglo
+        int arreglo [][]= generarArreglo(3, 3);
+        
+        char opcion;
+        do {
+            System.out.println("\n MENU EJERCICIO 1 ---");
+            System.out.println("a) Obtener el número que más se repite");
+            System.out.println("b) Obtener la cantidad de números pares ");
+            System.out.println("c) Obtener la cantidad de números impares");
+            System.out.println("d) Obtener la suma de los numeros en diagonal");
+            System.out.println("e) Obtener la cantidad de números multiplos de ");
+            System.out.println("x) SALIR");
+            System.out.print("Ingresa opcion pls: ");
+            opcion = leer.next().charAt(0);
 
+            switch (opcion) {
+                case 'a':
+                    sacarNumRepetido(arreglo);
+                    break;
 
-    //metodo natii :p
+                case 'b':
+                    sacarNumerosPares(arreglo);
+                    break;
+
+                case 'c':
+                    sacarNumerosImPares(arreglo);
+                    break;
+                case 'd':
+                    sacarSumaDiagonal(arreglo);
+                    break;
+                case 'e':
+                    cantidadMultiplos5(arreglo);
+                    break;
+                case 'x':
+                    System.out.println("saliendo del ejercicio 1...");
+                    break;
+                default:
+                    System.out.println("opcion no valida.");
+            }
+        } while (opcion != 'x');
+    }
+     
+     //generar un arreglo de int
+     public int[][] generarArreglo(int filas, int columnas) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arreglo = new int[filas][columnas];
+        System.out.println("Ingrese los numeros para el arreglo:");
+        
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print("Número [" + i + "][" + j + "]: ");
+                arreglo[i][j] = sc.nextInt();
+            }
+        }
+        return arreglo;
+    }
+
+     //Numeros que mas se repite
+     public int sacarNumRepetido(int[][] arreglo) {
+         int total =  arreglo.length * arreglo[0].length;
+         int [] todos = new int[total];
+         int index = 0;
+         
+       for (int i = 0; i < arreglo.length; i++) {
+             for (int j = 0; j < arreglo[i].length; j++) {
+                 todos[index++]= j;
+             }
+        }
+       
+       int repeticiones = 0;
+       int repetido = todos[0];
+       
+         for (int i = 0; i < todos.length; i++) {
+             int contador =0;
+             for (int j = 0; j < todos.length; j++) {
+                 if (todos[i] == todos [j]){
+                     contador ++;
+                 }
+                 if (contador > repeticiones){
+                     repeticiones = contador;
+                     repetido = todos[i];
+                 }
+                 
+             }
+             
+             
+             
+         }
+         return repetido;
+     }
+     
+     
+     //Numeros pares
+     public int sacarNumerosPares(int[][] arreglo) {
+         int contador = 0;
+         
+         for (int i = 0; i < arreglo.length; i++) {
+             for (int j = 0; j < arreglo[i].length; j++) {
+                 if (arreglo[i][j] % 2 == 0) {
+                contador++;
+            }
+        }
+    }
+    return contador;
+     }
+     
+     //Numeros pares
+     public int sacarNumerosImPares(int[][] arreglo) {
+         int contador = 0;
+         
+         for (int i = 0; i < arreglo.length; i++) {
+             for (int j = 0; j < arreglo[i].length; j++) {
+                 if (arreglo[i][j] % 2 != 0) {
+                contador++;
+            }
+        }
+    }
+    return contador;
+     }
+
+     //Suma diagonal
+     public int sacarSumaDiagonal(int[][] arreglo) {
+         int suma = 0;
+         
+         for (int i = 0; i < arreglo.length; i++) {
+             suma+= arreglo [i][i];
+         }
+         return suma;
+     }
+
+    //metodo natii :p multiplos de 5
     public int cantidadMultiplos5(int[][] arreglo) {
     int contador = 0;
     for (int i = 0; i < arreglo.length; i++) {
--- src/org/utl/ldsm304/vista/Principal.java
+++ src/org/utl/ldsm304/vista/Principal.java
@@ -4,6 +4,10 @@
  */
 package org.utl.ldsm304.vista;
 
+import java.util.Scanner;
+import org.utl.ldsm304.controlador.Controlador;
+import org.utl.ldsm304.controlador.ControladorCadena;
+
 /**
  *
  * @author IRIS Y NAT
@@ -14,7 +18,36 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        char opcion;
+        
+        
+
+        do {
+            System.out.println("\n MENU PRINCIPAL :) ");
+            System.out.println("1) Ejercicio 1");
+            System.out.println("2) Ejercicio 2");
+            System.out.println("x) Salir");
+            System.out.print("Opción: ");
+            opcion = leer.next().charAt(0);
+
+            switch (opcion) {
+                case '1':
+                    Controlador c = new Controlador();
+                    c.ejecutar(); 
+                    break;
+                case '2':
+                    ControladorCadena cc = new ControladorCadena();
+                    cc.ejecutar(); 
+                    break;
+                case 'x':
+                    System.out.println("saliendo...");
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+            }
+        } while (opcion != 'x');
+    }
     }
     
-}
+
